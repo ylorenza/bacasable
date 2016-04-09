80a939ed8e50044b46b1f5b3f258debb37b75992
Yannick Lorenzati <yannick.lorenzat@gmail.com>
2016-04-09 20:19:04 +0200

    first version of install arch

--- install_arch.sh
+++ install_arch.sh
@@ -24,16 +24,16 @@ dd if=/dev/zero of=/dev/sda bs=512 count=1
 sfdisk /dev/sda < sda.layout
 
 # format filesystem
-# mkfs.ext4 /dev/sda1
+mkfs.ext4 /dev/sda1
 
 # mount all the new part to /mnt
-#mount /dev/sda1 /mnt
+mount /dev/sda1 /mnt
 
 # install base package
-# pacstrap /mnt base base-devel
+pacstrap /mnt base base-devel
 
 # fstab
-#genfstab -U /mnt >> /mnt/etc/fstab
+genfstab -U /mnt >> /mnt/etc/fstab
 
 # local
 echo "en_US.UTF-8 UTF-8" > /mnt/etc/locale.gen
@@ -46,6 +46,22 @@ echo "KEYMAP=fr-pc" > /mnt/etc/vconsole.conf
 
 # Timezone
 arch-chroot /mnt ln -sf /usr/share/zoneinfo/Europe/Paris /etc/localtime
-hwclock --systohc --utc
+arch-chroot /mnt hwclock --systohc --utc
+
+# hostname
+echo "bacasable" > /mnt/etc/hostname
+
+arch-chroot /mnt systemctl enable dhcpcd.service
+
+arch-chroot /mnt pacman -S --noconfirm grub
+
+arch-chroot /mnt grub-install /dev/sda
+
+arch-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg
+
+
+umount -R /mnt
+
+echo "finish install of bacasable, please remove the usb key and reboot"
 
 exit 0
